01e59b4fb78ae55f204b79bab759e091519ccbad
Lizhilin <LZL61219@163.com>
2016-09-09 23:01:34 +0800

    2016年9月9日

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,5 @@
+import ReactDOM from 'react-dom'
+
 require('normalize.css/normalize.css');
 require('../styles/App.scss');
 //获取图片相关数据
@@ -14,7 +16,7 @@ imageDatas = (function getImageURL(imageDatasArr) {
 		singleImageData.imageURL = require('../images/'+ singleImageData.fileName)
 		imageDatasArr[i] = singleImageData;
 	}
-
+	console.log(imageDatasArr)
 	return imageDatasArr;
 })(imageDatas);
 
@@ -34,20 +36,60 @@ var ImgFigure  = React.createClass({
 })
 
 var GalleryByReactApp = React.createClass({
+	Constant:{//常量存储
+		centerPos:{ left:0, right:0},
+		hPosRange:{ leftSecX:[0,0], rightSecX:[0,0], y:[0,0]},//水平方向的取值范围
+		vPosRange:{ x:[0,0], topY:[0,0]}//垂直方向的取值范围
+	},
+
+	//组件加载后，为每张图片计算其位置的范围
+	componentDidMount: function () {
+		//首先拿到舞台的大小
+		var stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+			stageW = stageDOM.scrollWidth,
+			stageH = stageDOM.scrollHeight,
+			halfStageW = Math.ceil(stageW /2),
+			halfStageH = Math.ceil(stageH /2);
+		console.log(stageDOM)
+
+		//拿到一个 imageFigure 的大小
+		var imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
+			imgW = imgFigureDOM.scrollWidth,
+			imgH = imgFigureDOM.scrollHeight,
+			halfImgW = Math.ceil(imgW/2),
+			halfImgH = Math.ceil(imgH/2);
+		//计算中心图片的位置点
+		this.Constant.centerPos ={
+			left: halfStageW - halfImgW,//舞台的一半宽减去图片的一半宽
+			top: halfStageH - halfImgH
+		}
+		this.Constant.hPosRange.leftSecX[0] = - halfImgW;
+		this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW *3;
+		this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+		this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+		this.Constant.hPosRange.y[0] = - halfImgH;
+		this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+		this.Constant.vPosRange.topY[0] = - halfImgH;
+		this.Constant.vPosRange.topY[1] = halfStageH - halfImgH *3;
+		this.Constant.vPosRange.x[0] = halfImgW - imgW;//最小值
+		this.Constant.vPosRange.x[1] = halfImgW;//最大值
+	},
+
 	render: function () {
 
 		var controllerUnits = [],
 			imgFigures = [];
 
 		imageDatas.forEach(function (value,index) {
-			imgFigures.push( <ImgFigure key={index} data={value} /> );
+			imgFigures.push( <ImgFigure key={index} data={value} ref={'imgFigure'+index}/> );
 		});
 		//for(var i in imageDatas){
 		//	imgFigures.push( <ImgFigure key={i} data={imageDatas[i]} /> );
 		//}
 
 		return(
-			<section className="stage">
+			<section className="stage" ref='stage'>
 				<section className="ima-sec">
 					{imgFigures}
 				</section>
